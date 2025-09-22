00e3818545168d88a09a50936d1a3a327a3f73c0
GeorgeM2564 <giorgionero03@gmail.com>
2025-09-22 21:03:39 +0300

    Add files via upload

---
+++ xaris.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLISENSED
+pragma solidity >=0.8.30;
+
+contract Xarilaos {
+    uint256 public xarilaos_bitches;
+    string public xarilaos_first_date = "as-soon-as-posssible-i-hope-twin";
+    string public xarilaos_first_bitch_name= "Maria";
+    string public xarilaos_bitch_name;
+}
+
+function GetXarilaosBitches(
+    string memory xarilaos_first_bitch_name,
+    uint256 xarilaos_bitchespure 
+) {
+    string memory xarilaos_first_bitch_name = "Maria";
+    uint256 xarilaos_bitches = 1;
+    return string;
+}
